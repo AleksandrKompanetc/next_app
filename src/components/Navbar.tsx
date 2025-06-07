c558ceaeccd538c81125a07c280d31f573e8ac71
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-07 20:03:00 +0300

    added Home component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ export const Navbar = () => {
     <nav className="flex gap-4 bg-white p-4 shadow mb-4">
       <div className="text-2xl font-bold">Logo</div>
       <div>
-      <Link href="/" className="text-blue-600 hover:underline">Home</Link>
-      <Link href="/about" className="text-blue-600 hover:underline">About</Link>
-      <Link href="/contact" className="text-blue-600 hover:underline">Contact</Link>
+        <Link href="/" className="text-blue-600 hover:underline">Home</Link>
+        <Link href="/about" className="text-blue-600 hover:underline">About</Link>
+        <Link href="/contact" className="text-blue-600 hover:underline">Contact</Link>
       </div>
     </nav>
   )
