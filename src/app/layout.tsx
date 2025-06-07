0ad5d8283d19fe4c2d614bbc4e1ffb11e77c2392
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-07 19:49:49 +0300

    added logo for Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Navbar } from "@/components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className='bg-gray-100 text-gray-900'>
         <main className="p-4">
+          <Navbar />
           {children}
         </main>
       </body>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,12 @@ import Link from "next/link"
 export const Navbar = () => {
   return (
     <nav className="flex gap-4 bg-white p-4 shadow mb-4">
-      <Link href="/"></Link>
+      <div className="text-2xl font-bold">Logo</div>
+      <div>
+      <Link href="/" className="text-blue-600 hover:underline">Home</Link>
+      <Link href="/about" className="text-blue-600 hover:underline">About</Link>
+      <Link href="/contact" className="text-blue-600 hover:underline">Contact</Link>
+      </div>
     </nav>
   )
 }
