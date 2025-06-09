785a8731230acb11b3aed9613cdaa542009bb74e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-09 23:27:27 +0300

    added states for CreateUser

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,7 +2,8 @@ import { useState } from "react"
 import { useRouter } from "next/router"
 
 export default function CreateUser() {
-  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
   return (
     <div>
       <h1>Create</h1>
