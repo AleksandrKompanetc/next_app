a369f984e982e80a779276bb086e9a687987fe5d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-09 23:29:45 +0300

    added changes for CreateUser

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -4,9 +4,12 @@ import { useRouter } from "next/router"
 export default function CreateUser() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const router = useRouter()
+
   return (
-    <div>
+    <div className="p-6 max-w-md mx-auto">
       <h1>Create</h1>
+      <form action=""></form>
     </div>
   )
 }
