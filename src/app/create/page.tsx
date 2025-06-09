a482a71fbd1ff63407f591a2d8ff6a2abc1bc8bd
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-09 23:26:00 +0300

    changed Create component

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,8 @@
-export default function Create() {
+import { useState } from "react"
+import { useRouter } from "next/router"
+
+export default function CreateUser() {
+  
   return (
     <div>
       <h1>Create</h1>
