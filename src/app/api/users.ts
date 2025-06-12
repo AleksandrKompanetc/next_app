3fb5d15d7566d2da8b93e4e1e399e5166b5ebf73
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-12 23:04:18 +0300

    changed users file in api

--- src/app/api/users.ts
+++ src/app/api/users.ts
@@ -1,4 +1,17 @@
 let users = [
   {id: 1, name: 'Alice', email: 'alice@example.com'},
   {id: 2, name: 'Bob', email: 'bob@example.com'}
-]
+];
+
+export default function handler(req: any, res: any) {
+  if (req.method === 'GET') {
+    return res.status(200).json(users)
+  }
+
+  if (req.method === 'POST') {
+    const newUser = {id: Date.now(), ...req.body}
+    users.push(newUser)
+    return res.status(201).json(newUser)
+  }
+  res.status(405).end
+}
