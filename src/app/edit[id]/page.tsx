68ce5ab590441d85498ec4c5f43a72fba2ef0baf
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-13 23:18:42 +0300

    added dependencies for EditUser component

--- src/app/edit[id]/page.tsx
+++ src/app/edit[id]/page.tsx
@@ -2,5 +2,8 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 export default function EditUser() {
-  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const router = useRouter()
+  const {id} = router.query
 }
