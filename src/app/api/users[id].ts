50e0950f5a0a5904e7ff2d48d7e16c46da0914f0
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-16 23:07:57 +0300

    added case DELETE

--- src/app/api/users[id].ts
+++ src/app/api/users[id].ts
@@ -10,11 +10,27 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     case 'GET':
       res.status(200).json(users)
       break
+
     case 'POST':
       const {name, email} = req.body
       if (!name || !email) {
         res.status(400).json({message: 'Name and email are required'})
         return
+      }
+    
+      const newUser = {
+        id: users.length + 1,
+        name,
+        email,
+      }
+      users.push(newUser)
+      res.status(201).json(newUser)
+      break
+
+    case 'DELETE':
+      const id = parseInt(req.query.id as string)
+      if(!id) {
+        
       }
   }
 }
