cce73590b39b545c618f62f6663495fec729bb74
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-21 23:52:45 +0300

    changed connection firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from "@/components/Todo";
 import { db } from './firebase';
-import { query, collection } from 'firebase/firestore';
+import { query, collection, onSnapshot } from 'firebase/firestore';
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -22,6 +22,14 @@ export default function Home() {
 
   useEffect(() => {
     const q = query(collection(db, "todos"))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let todosArr = []
+      querySnapshot.forEach((doc) => {
+        todosArr.push({...doc.data(), id: doc.id})
+      })
+      setTodos(todosArr)
+    })
+    return () => unsubscribe()
   }, [])
 
   return (
