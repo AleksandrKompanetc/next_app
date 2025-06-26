9a2c606ee7d6682608a3b0d66da75faa50853f01
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-26 23:30:11 +0300

    created Navbar component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p className='text-2xl font-bold text-purple-600 bg-lime-500'>Film</p>
-    </div>
+    <>
+      <h1 className='text-white'>Hello</h1>
+    </>
   );
 }
 
