29f2c0ae17a50dd3ce1a848719a6391ddb055d3d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-26 23:57:30 +0300

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes path="/" element={<Home />} />
     </>
   );
 }
