a92ccdf44f8daef7df436394a1ec45aac9dee7dd
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-28 23:31:39 +0300

    added useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const Main = () => {
   const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    
+  })
 
   return (
     <div>
