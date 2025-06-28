8cb255a21fa091ae850e33dcc3ff55ee73b8c664
AleksandrKompanetc <moneymacker7@gmail.com>
2025-06-28 23:33:45 +0300

    added axios

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 const Main = () => {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
-    
+    axios.get()
   })
 
   return (
