0c1e2537994728599d1e68c53aa03187d279feb0
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-03 23:49:02 +0300

    changed UserForm

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -4,6 +4,9 @@ import { User } from '../types'
 import { submitUser } from '../services/api'
 
 export const UserForm = () => {
+  const { register, handleSubmit, formState: { errors} } = useForm<User>()
+  const [submited, setSubmited] = useState(false)
+
   return (
     <div className='max-w-md mx-auto mt-10 p-4 bg-white rounded-xl shadow'>
       <h2 className='text-2xl font-bold mb-4'>User Form</h2>
