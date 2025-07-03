806c3efce1f0b66929280f4fca8fd14969b15b53
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-03 23:41:05 +0300

    created function submitUser

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import { User } from '../types'
 
 const API_URL = 'https://jsonplaceholder.typicode.com/users'
 
+export const submitUser = async (user: User) => {
+  const response = await axios.post(API_URL, user)
+  return response.data
+}
