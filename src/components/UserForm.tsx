93a097595359f6fb3de095f26e687d48e89d3181
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-04 23:46:50 +0300

    added input for email

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -22,8 +22,13 @@ export const UserForm = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className='mb-4'>
           <label className='block mb-1'>Name</label>
-          <input {...register('name', { required: true})} className='w-full boredr rounded px-3 py-2' />
+          <input {...register('name', { required: true })} className='w-full border rounded px-3 py-2' />
           {errors.name && <p className='text-red-500 text-sm'>Name is required</p>}
+        </div>
+        <div className='mb-4'>
+          <label className='block mb-1'>Email</label>
+          <input {...register('email', { required: true })} className='w-full border rounded px-3 py-2' />
+          {errors.email && <p className='text-red-500 text-sm'>Email is required</p>}
         </div>
       </form>
     </div>
