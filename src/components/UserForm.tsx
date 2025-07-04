f1a13cf4c376bc2f8a23fd85f826b0953a98bb9f
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-04 23:35:49 +0300

    added form for UserForm component

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -19,6 +19,11 @@ export const UserForm = () => {
   return (
     <div className='max-w-md mx-auto mt-10 p-4 bg-white rounded-xl shadow'>
       <h2 className='text-2xl font-bold mb-4'>User Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='mb-4'>
+          
+        </div>
+      </form>
     </div>
   )
 }
