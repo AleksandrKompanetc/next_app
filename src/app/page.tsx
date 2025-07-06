983e4d89d6388ce8965ab045c48ec326ac5fd583
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-06 23:34:16 +0300

    created components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function QuizPage() {
   return (
-    
+    <div className="max-w-xl mx-auto p-4">
+      <h1 className="text-2xl text-purple-600 font-bold mb-4">Quiz App</h1>
+    </div>
   );
 }
