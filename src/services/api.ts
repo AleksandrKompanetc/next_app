fa777a0b6a4cd8b3b34f3b230322dc324c440902
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-06 23:25:11 +0300

    created new project

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import { User } from '../types'
-
-const API_URL = 'https://jsonplaceholder.typicode.com/users'
-
-export const submitUser = async (user: User) => {
-  const response = await axios.post(API_URL, user)
-  return response.data
-}
