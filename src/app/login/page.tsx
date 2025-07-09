fc332bfd6061cd5fce0a89ac825a6c962f22f33b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-09 23:57:12 +0300

    changed LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,5 +4,11 @@ import { useForm } from "react-hook-form"
 export default function LoginPage() {
   const { register, handleSubmit } = useForm()
 
-  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <input type="text" />
+      <input type="text" />
+      <button></button>
+    </form>
+  )
 }
