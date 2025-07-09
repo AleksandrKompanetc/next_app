8b3c0608017f55a7055cddbae2fc06bf0e1f65fe
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-09 23:47:42 +0300

    created HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 
-export default function QuizPage() {
+
+export default function HomePage() {
   return (
     <div className="max-w-xl mx-auto p-4">
-      <h1 className="text-2xl text-purple-600 font-bold mb-4">Quiz App</h1>
+      <h1 className="text-2xl text-purple-600 font-bold">Welcome!</h1>
+      <p>It is HomePage</p>
     </div>
   );
 }
