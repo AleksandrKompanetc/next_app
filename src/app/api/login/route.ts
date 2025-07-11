3c884cfa50e97c1489e004e03fc1ffa999589386
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-11 23:57:47 +0300

    changed route

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  
+  const {email, password} = await req.json()
+
+  if(email === 'admin@example.com' && password === '123') {
+    
+  }
 }
