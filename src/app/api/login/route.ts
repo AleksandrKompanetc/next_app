3dbae5206d2b5e16e0062bd4d7f8e79081477b01
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-12 23:48:16 +0300

    changed function Post

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,6 +4,10 @@ export async function POST(req: Request) {
   const {email, password} = await req.json()
 
   if(email === 'admin@example.com' && password === '123') {
-    
+    const response = NextResponse.json({success: true})
+    response.cookies.set('token', 'mock-jwt-token')
+    return response
   }
+
+  return NextResponse.json({error: 'Wrong data'}, {status: 401})
 }
