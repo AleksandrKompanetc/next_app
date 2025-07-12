e26527a0e75627512af2b7106ff4c467b969453e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-12 23:59:21 +0300

    changed LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
 
 export default function LoginPage() {
   const { register, handleSubmit } = useForm()
