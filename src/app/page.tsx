410bc38b2377b24a8dae9d0033fa8aa3a8b8c8e0
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-14 23:51:41 +0300

    added nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import Image from "next/image"
+import {Globe} from "lucide-react"
 
 export default function Home() {
   return (
@@ -11,7 +12,11 @@ export default function Home() {
           <div className="flex items-center gap-2 ">
             <Globe className="h-5 w-5 text-blue-400" />
             <span className="text-sm font-medium">META-PWA</span>
+            <div className="ml-2 rounded-full border border-zinc-800 bg-zinc-900/80 px-2 py-0.5 text-xs">v1.0</div>
           </div>
+          <nav className="hidden md:flex md:gap-6 absolute left-1/2 transform -translate-x-1/2">
+            
+          </nav>
         </div>
       </header>
     </div>
