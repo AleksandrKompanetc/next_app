d2409172c51bfc8e9f4c2e1b2cbbe57931d22af0
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-16 23:34:58 +0300

    added section features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        
+        <section id="features" className=""></section>
       </main>
     </div>
   );
