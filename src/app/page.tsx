018ecec1b4e97f39e5f80b38431368588d11489b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-07-24 23:56:32 +0300

    changed contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -369,7 +369,12 @@ export default function Home() {
           </div>
         </section>
         <section id="contact" className="relative bg-gradient-to-b from-zinc-900/95 to-black py-30 md:py-32">
-          <div className="absolute inset-0"></div>
+          <div className="absolute inset-0 bg-[radial-gradient(circle at 59% 30%,rgba(59,130,246,0.05),transparent 0%)]"></div>
+          <div className="container mx-auto px-4 md:px-6 relative z-10">
+            <div className="grid gap-12 md:grid-cols-2">
+
+            </div>
+          </div>
         </section>
       </main>
     </div>
